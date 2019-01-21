8a894a93b8a0e5e1bacdef801ceb7a73d608e993
271920545@qq.com <271920545@qq.com>
2019-01-21 16:17:17 +0800

    Loader

---
+++ src/Deux/Library/Loader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by WanDeHua.
+ * User: WanDeHua 
+ * Email:271920545@qq.com
+ * Date: 2019/1/21
+ * Time: 16:13
+ */
+namespace Deux\Library;
+
+class Loader {
+    public function index(){
+        echo 'Deux Loader';
+    }
+}
